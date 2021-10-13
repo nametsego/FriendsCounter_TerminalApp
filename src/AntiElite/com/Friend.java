764755920eb61cde56f65beeb08dc761c81cd9a9
Nametsego <Nametsegothakadu@gmail.com>
2021-10-13 04:06:44 +0200

    Complete code setup for master branch

---
+++ src/AntiElite/com/Friend.java
@@ -0,0 +1,15 @@
+package AntiElite.com;
+
+public class Friend {
+    String name;
+    static int numberOfFriends;
+
+    Friend(String name){
+        this.name = name;
+        numberOfFriends++;
+    }
+
+    static void displayFriends(){
+        System.out.println("You have " + numberOfFriends + " friends in your life");
+    }
+}
---
+++ src/AntiElite/com/Main.java
@@ -0,0 +1,17 @@
+package AntiElite.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Friend friend1 = new Friend("Nametsego");
+        Friend friend2 = new Friend("Snapi");
+        Friend friend3 = new Friend("Nemesis");
+        Friend friend4 = new Friend("MostOddSoul");
+        Friend friend5 = new Friend("FunkyFresh");
+        Friend friend6 = new Friend("Sname");
+
+        Friend.displayFriends();
+
+    }
+}
